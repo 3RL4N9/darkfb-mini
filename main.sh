a353449d3d2635335d481a66a58198022a1e78a6
Erlang <71025266+3RL4N9@users.noreply.github.com>
2020-10-23 22:45:53 +0700

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,21 @@
+pkg upgrade
+pkg update
+pkg install python
+pkg install python2
+pkg install git
+pkg install ruby
+pkg install nano
+pkg install figlet
+gem install lolcat
+pip install --upgrade pip
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+pip2 install futures
+pip2 install tqdm
+figlet Aku | lolcat
+figlet keren | lolcat
+echo -e "Selesai Bos" | lolcat
+sleep 5
+python2 mini.pyc
+
